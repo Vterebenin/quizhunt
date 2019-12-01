5f0bc8b92289337969bc19cb98a20084c028f5c1
Vterebenin <terebenin.work@gmail.com>
2019-12-01 16:18:01 +0300

    feat: create quiz app

--- quizhunt/settings.py
+++ quizhunt/settings.py
@@ -115,6 +115,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
